4694673121713951203db2a329b80be41a662aad
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 <l.leroty@gmail.com>
2022-08-17 19:50:31 +0200

    add nop function

--- nop.c
+++ nop.c
@@ -3,7 +3,7 @@
 #include "monty.h"
 
 /**
- * nop - A function that litterally doesn't do anything
+ * nop - A function that litterally doesn't do anything.
  * @stack: pointer to the top of the stack
  * @line_number: line number where nothing happens
  * Return: void
