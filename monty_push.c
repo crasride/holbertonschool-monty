f9de44dc2aa87abd7b2ba1ce8afd3dfad2707bd9
crasride <jose.dfernandez@yahoo.fr>
2022-08-18 10:05:34 +0200

    fix task 3

--- monty_push.c
+++ monty_push.c
@@ -11,8 +11,11 @@ void monty_push(stack_t **stack, unsigned int line_number)
 	char *val;
 	int number, count, check = 0;
 
-	val = strtok(NULL, "\n\t\r ");
+/*	printf("A\n"); */
+	val = strtok(NULL, "\n\t\r");
 
+/*	printf("<B\n"); */
+/*	printf("%p\n", val); */
 	for (count = 0; val[count]; count++)
 	{
 		if (val[count] == '-' && count == 0)
@@ -20,11 +23,13 @@ void monty_push(stack_t **stack, unsigned int line_number)
 		if (isdigit(val[count]) == 0)
 			check = 1;
 	}
+/*	printf("AB\n");*/
 	if (val == NULL || check)
 	{
 		fprintf(stderr, "L%u: usage: push integer\n", line_number);
 		exit(EXIT_FAILURE);
 	}
+/*	printf("C\n"); */
 	number = atoi(val);
 	add_stack(stack, number);
 }
